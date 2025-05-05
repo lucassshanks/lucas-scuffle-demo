8bfc1df78239c7b820fa7704a2c4c03fa9e2ad52
Lucas Shanks <lucassshanks@gmail.com>
2025-05-05 00:50:03 -0400

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,180 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Stream, StreamPlayerApi } from "@cloudflare/stream-react";
+import { ReactCompareSlider } from "react-compare-slider";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const video1 = "";
+  const video2 = "";
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+  const [isLogoLoaded, setIsLogoLoaded] = useState(false);
+  const [isDescriptionLoaded, setIsDescriptionLoaded] = useState(false);
+  const [areVideosReady, setAreVideosReady] = useState(false);
+  const [isVideoDescriptionLoaded, setIsVideoDescriptionLoaded] =
+    useState(false);
+
+  const stream1Ref = useRef<StreamPlayerApi>(undefined);
+  const stream2Ref = useRef<StreamPlayerApi>(undefined);
+  const compareSliderRef = useRef<HTMLElement>(null);
+
+  const handleLogoLoad = () => {
+    setIsLogoLoaded(true);
+    setTimeout(() => setIsDescriptionLoaded(true), 1000);
+  };
+
+  // Only show player once videos are loaded to prevent flickering on video/slider load
+  const handleVideoLoad = () => {
+    if (stream1Ref.current && stream2Ref.current) {
+      setAreVideosReady(true);
+      setTimeout(() => setIsVideoDescriptionLoaded(true), 1000);
+    }
+  };
+
+  const togglePlayback = async () => {
+    if (!stream1Ref.current || !stream2Ref.current) return;
+
+    const action = isPlaying ? "pause" : "play";
+    Promise.all([
+      stream1Ref.current[action](),
+      stream2Ref.current[action](),
+    ]).then(() => {
+      setIsPlaying((prev) => !prev);
+    });
+  };
+
+  const toggleAudioPlayer1 = () => {
+    if (!stream1Ref.current || !stream2Ref.current) return;
+    stream1Ref.current.muted = !isMuted;
+    setIsMuted((prev) => !prev);
+  };
+
+  const toggleFullscreen = async () => {
+    if (!compareSliderRef.current) return;
+    if (!document.fullscreenElement) {
+      await compareSliderRef.current.requestFullscreen();
+    } else {
+      await document.exitFullscreen();
+    }
+  };
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <header
+        className={`${styles.main} ${styles.loadAnimation} ${isLogoLoaded ? styles.visible : ""}`}
+      >
+        <div className={styles.titleContainer}>
+          <Image
+            className={styles.logo}
+            src="/bear.svg"
+            alt="Bear"
+            width={130}
+            height={80}
+            priority
+            onLoad={handleLogoLoad}
+          />
+          <h1>Scuffle Demo</h1>
         </div>
+        <p
+          className={`${styles.description} ${styles.loadAnimation} ${isDescriptionLoaded ? styles.visible : ""}`}
+        >
+          Compare the differences between two video streams. Drag the slider to
+          reveal the differences in quality and performance.
+        </p>
+      </header>
+      <main className={styles.sectionsWrapper}>
+        <section
+          ref={compareSliderRef}
+          className={`${styles.loadAnimation} ${areVideosReady ? styles.visible : ""}`}
+          style={{ width: "100%" }}
+        >
+          <div className={styles.controls}>
+            <button onClick={togglePlayback} className={styles.controlButton}>
+              {isPlaying ? "Pause" : "Play"}
+            </button>
+            <button
+              onClick={toggleAudioPlayer1}
+              className={styles.controlButton}
+            >
+              {isMuted ? "Unmute" : "Mute"}
+            </button>
+            <button onClick={toggleFullscreen} className={styles.controlButton}>
+              Toggle Fullscreen
+            </button>
+          </div>
+          <ReactCompareSlider
+            style={{
+              width: "100%",
+              height: "100%",
+              aspectRatio: "16/9",
+            }}
+            onlyHandleDraggable={true}
+            itemOne={
+              <div style={{ width: "100%", height: "100%" }}>
+                <Stream
+                  streamRef={stream1Ref}
+                  src={video1}
+                  responsive
+                  muted={isMuted}
+                  onLoadedData={handleVideoLoad}
+                />
+              </div>
+            }
+            itemTwo={
+              <div style={{ width: "100%", height: "100%" }}>
+                <Stream
+                  streamRef={stream2Ref}
+                  src={video2}
+                  responsive
+                  muted
+                  onLoadedData={handleVideoLoad}
+                />
+              </div>
+            }
+          />
+          <div className={styles.comparisonLabels}>
+            <span>H.264</span>
+            <hr className={styles.dividerLine} />
+            <span>AV1</span>
+          </div>
+        </section>
+        <section
+          className={`${styles.loadAnimation} ${isVideoDescriptionLoaded ? styles.visible : ""}`}
+        >
+          <p style={{ marginBottom: "2rem" }}>
+            <strong>H.264</strong> (also known as AVC), the most common form of
+            video encoding, started from <strong>2003</strong> and has has
+            become the most popular video encoding format. It's most loved for
+            its <strong>universal hardware support</strong> and{" "}
+            <strong>efficient real-time encoding capabilities</strong> while
+            still delivering high quality. <br />
+            <br /> This 1080p 60fps requires approximately{" "}
+            <strong>6-8 Mbps bandwidth</strong> to users while maintaining
+            professional streaming quality.
+          </p>
+          <p style={{ marginBottom: "1rem" }}>
+            <strong>AV1</strong> is the next-generation form of video encoding
+            developed in the last 5 years, but only recently has hardware become
+            reasonable to do this encoding on the fly. This{" "}
+            <strong>open-source solution</strong> is already integrated into
+            YouTube and Netflix, offering up to{" "}
+            <strong>50% better compression efficiency</strong> compared to H.264
+            <br />
+            <br />
+            This 1080p 60fps with only <strong>3-4 Mbps bandwidth</strong> to
+            reach users with visibly improved quality and reduced artifacts even
+            at 2K bitrate.
+          </p>
+          <p style={{ fontSize: "0.9rem", color: "#666", fontStyle: "italic" }}>
+            Note: When pausing the video, you might notice some graininess. This
+            is due to video compression using inter-frame prediction, where some
+            frames depend on other frames for their full quality.
+          </p>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
